15693b782b39b263cfa169b876b4a42348a198cf
Matthew Hughes <matt.h17@yahoo.com>
2024-06-03 20:40:14 +0000

    Refactored post and profile to generic views

--- drf_api/followers/views.py
+++ drf_api/followers/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Followers
+from .serializers import FollowerSerializer
 
 # Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Followers.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    serializer_class = FollowerSerializer
+    queryset = Followers.objects.all()
+    permission_classes = [IsOwnerOrReadOnly]
